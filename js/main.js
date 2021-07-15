d8d61d6e960d42354d8ddc353ce58d308a28b77e
ManuelInfante <infantemanuel.mail@gmail.com>
2021-07-15 17:07:45 +0200

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome;
+nome = prompt('Inserisci il tuo nome');
+
+var cognome;
+cognome = prompt('Inserisci il tuo cognome');
+
+var colore;
+colore = prompt('inserisci il tuo colore preferito');
+
+document.getElementById('pass').innerHTML = nome + cognome + colore + '21';
